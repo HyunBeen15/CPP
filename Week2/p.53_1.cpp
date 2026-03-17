c966384dca468a7c75aa5220d8632681b4ef697a
HyunBeen15 <chbn0105@gmail.com>
2026-03-17 20:31:06 +0900

    p.53_1.cpp

---
+++ Week2/p.53_1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+
+printf(" _____  _   _ ______ \n");
+printf("/  __ \| | | || ___ \ \n");
+printf("| /  \/| |_| || |_/ / \n");
+printf("| |    |  _  || ___ \ \n");
+printf("| \__/\| | | || |_/ / \n");
+printf(" \____/\_| |_/\____/ \n");
+
+return 0;                       
+}
